778133f5ea00ea93d55a5de10074de36e4fca914
Niklas Tasler <niklastasler@gmail.com>
2024-11-24 19:01:30 +0100

    feat: add angular module

---
+++ modules/home-manager/angular/default.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.tarow.angular;
+in
+{
+  options.tarow.angular = {
+    enable = lib.options.mkEnableOption "Angular";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.vscode = {
+      home.packages = with pkgs;[ nodePackages."@angular/cli" ];
+      extensions = with pkgs.vscode-marketplace; with pkgs.vscode-marketplace-release;
+        [
+          angular.ng-template
+          bradlc.vscode-tailwindcss
+          formulahendry.auto-rename-tag
+        ];
+      userSettings = { };
+    };
+  };
+}
