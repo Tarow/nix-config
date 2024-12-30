c84a07283183cfc8e4fb42c2ca6ef8b8dd841e7f
Niklas Tasler <niklastasler@gmail.com>
2024-12-30 09:50:36 +0100

    refactor: use run command for activation script

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -76,10 +76,10 @@ in
     # See https://github.com/NixOS/nixpkgs/issues/12757#issuecomment-2253490852
     home.activation.copyDesktopFiles = lib.hm.dag.entryAfter [ "installPackages" ] ''
       if [ -d "${config.home.homeDirectory}/.nix-profile/share/applications" ]; then
-        rm -rf ${config.home.homeDirectory}/.local/share/applications
-        mkdir -p ${config.home.homeDirectory}/.local/share/applications
+        run rm -rf ${config.home.homeDirectory}/.local/share/applications
+        run mkdir -p ${config.home.homeDirectory}/.local/share/applications
         for file in ${config.home.homeDirectory}/.nix-profile/share/applications/*; do
-          ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
+          run ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
         done
       fi
     '';
