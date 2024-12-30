8e7e90914fccd72fd80bbb08d193e9fea8afc32a
Niklas Tasler <niklastasler@gmail.com>
2024-12-30 09:45:03 +0100

    refactor: remove outdated docker test modules

--- modules/home-manager/docker/traefik/labels.nix
+++
@@ -1,6 +0,0 @@
-{ config, name, port, lib }:
-[
-  "traefik.http.routers.${name}.rule=Host(`${name}.${config.tarow.docker.traefik.domain}`)"
-  "traefik.http.routers.${name}.entrypoints=websecure"
-  "traefik.http.routers.${name}.middlewares=private-chain@file"
-] ++ lib.lists.optional (port != null) "traefik.http.services.${name}.loadbalancer.server.port=${toString port}"
