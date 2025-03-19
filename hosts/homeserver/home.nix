1d675599c459d1d28b2a2a56f2f1f628b60fa3c7
niklastasler@gmail.com <niklastasler@gmail.com>
2025-03-19 11:53:49 +0100

    feat: pangolin stack

--- hosts/homeserver/home.nix
+++ hosts/homeserver/home.nix
@@ -20,20 +20,22 @@
     stacks = {
       enable = true;
       adguard.enable = true;
-      audiobookshelf.enable = true;
-      calibre.enable = true;
-      dozzle.enable = true;
-      dockdns.enable = true;
-      filebrowser.enable = true;
-      healthchecks.enable = true;
-      homepage.enable = true;
-      streaming.enable = true;
-      stirling-pdf.enable = true;
-      immich.enable = true;
-      paperless.enable = true;
-      wg-easy.enable = true;
+      #audiobookshelf.enable = true;
+      #calibre.enable = true;
+      #dozzle.enable = true;
+      #dockdns.enable = true;
+      #filebrowser.enable = true;
+      #healthchecks.enable = true;
+      #homepage.enable = true;
+      #streaming.enable = true;
+      #stirling-pdf.enable = true;
+      #immich.enable = true;
+      #paperless.enable = true;
+      #wg-easy.enable = true;
+      pangolin.enable = true;
+      pangolin.domain = "ntasler.de";
       traefik = {
-        enable = true;
+        #  enable = true;
         domain = "ntasler.de";
       };
     };
