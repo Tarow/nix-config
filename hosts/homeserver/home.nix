d27eda64b924cbf0f1a8b0a37c72c8422e150011
niklastasler@gmail.com <niklastasler@gmail.com>
2025-03-19 11:55:19 +0100

    chore: disable pangolin

--- hosts/homeserver/home.nix
+++ hosts/homeserver/home.nix
@@ -20,22 +20,20 @@
     stacks = {
       enable = true;
       adguard.enable = true;
-      #audiobookshelf.enable = true;
-      #calibre.enable = true;
-      #dozzle.enable = true;
-      #dockdns.enable = true;
-      #filebrowser.enable = true;
-      #healthchecks.enable = true;
-      #homepage.enable = true;
-      #streaming.enable = true;
-      #stirling-pdf.enable = true;
-      #immich.enable = true;
-      #paperless.enable = true;
-      #wg-easy.enable = true;
-      pangolin.enable = true;
-      pangolin.domain = "ntasler.de";
+      audiobookshelf.enable = true;
+      calibre.enable = true;
+      dozzle.enable = true;
+      dockdns.enable = true;
+      filebrowser.enable = true;
+      healthchecks.enable = true;
+      homepage.enable = true;
+      streaming.enable = true;
+      stirling-pdf.enable = true;
+      immich.enable = true;
+      paperless.enable = true;
+      wg-easy.enable = true;
       traefik = {
-        #  enable = true;
+        enable = true;
         domain = "ntasler.de";
       };
     };
