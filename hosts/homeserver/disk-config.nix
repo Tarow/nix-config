c6536ebed46a14e66dad26476faa5c1742234e2c
Niklas Tasler <niklastasler@gmail.com>
2025-03-25 22:17:11 +0100

    refactor: homeserver disk config

--- hosts/homeserver/disk-config.nix
+++ hosts/homeserver/disk-config.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       ssd = {
-        device = "/dev/sda";
+        device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_500GB_S3Z2NB1KA22617Z";
         type = "disk";
         content = {
           type = "gpt";
@@ -24,6 +24,27 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+      hdd = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-WDC_WD40EFRX-68N32N0_WD-WCC7K0VJ5YVS";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/hdd1";
+                mountOptions = [
+                  "defaults"
+                  "nofail"
+                ];
               };
             };
           };
