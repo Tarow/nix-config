50d59605af7ca60ba3abc4fd8feaf29f4fd3978a
Niklas Tasler <niklastasler@gmail.com>
2025-05-24 12:05:33 +0200

    feat: add samba module

---
+++ modules/nixos/samba/default.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  options,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.tarow.samba;
+in {
+  options.tarow.samba = {
+    enable = lib.options.mkEnableOption "Samba";
+    extraSettings = options.services.samba.settings;
+  };
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      samba = {
+        enable = true;
+        openFirewall = true;
+        package = pkgs.samba4Full;
+        # ^^ `samba4Full` is compiled with avahi, ldap, AD etc support (compared to the default package, `samba`
+        # Required for samba to register mDNS records for auto discovery
+        # See https://github.com/NixOS/nixpkgs/blob/592047fc9e4f7b74a4dc85d1b9f5243dfe4899e3/pkgs/top-level/all-packages.nix#L27268
+        settings =
+          {
+            global = {
+              "workgroup" = "WORKGROUP";
+              "server string" = "smbnix";
+              "netbios name" = "smbnix";
+              "security" = "user";
+
+              "guest account" = "nobody";
+              "map to guest" = "bad user";
+            };
+          }
+          // cfg.settings;
+
+        extraConfig = ''
+          server smb encrypt = required
+          # ^^ Note: Breaks `smbclient -L <ip/host> -U%` by default, might require the client to set `client min protocol`?
+          server min protocol = SMB3_00
+        '';
+      };
+      avahi = {
+        publish.enable = true;
+        publish.userServices = true;
+        # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+
+        enable = true;
+        openFirewall = true;
+      };
+      samba-wsdd = {
+        # This enables autodiscovery on windows since SMB1 (and thus netbios) support was discontinued
+        enable = true;
+        openFirewall = true;
+      };
+    };
+  };
+}
