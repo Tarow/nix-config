cf108c4cc54b836f337d1859449b4f33c49e486e
Niklas Tasler <niklastasler@gmail.com>
2026-04-04 08:39:51 +0200

    feat: podman cleanup systemd job

--- modules/home-manager/podman/default.nix
+++ modules/home-manager/podman/default.nix
@@ -10,6 +10,24 @@ in {
   config = lib.mkIf cfg.enable {
     services.podman.enable = true;
 
+    systemd.user = {
+      timers."podman-cleanup" = {
+        Timer = {
+          OnCalendar = "03:00";
+          Persistent = true;
+        };
+        Install = {
+          WantedBy = ["timers.target"];
+        };
+      };
+      services."podman-cleanup" = {
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${lib.getExe config.services.podman.package} system prune -af";
+        };
+      };
+    };
+
     programs.fish.shellAbbrs = {
       pl = "podman logs";
       plf = "podman logs -f";
