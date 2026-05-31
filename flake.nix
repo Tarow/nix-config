dbf800f0a84b97f6830f045c4c0a565bb52e852e
Niklas Tasler <niklastasler@gmail.com>
2026-05-31 10:15:04 +0200

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS and Home Manager Configuration Flake for my Hosts";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
@@ -13,7 +13,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       #url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
